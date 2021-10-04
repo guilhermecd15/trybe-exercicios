bb2b1ca982f330eee24a633ae2503bd85c3c9f8f
guilhermecd15 <guilhermecd15@gmail.com>
2021-10-04 18:18:04 -0300

    Primeiro dia de React

---
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/hello-react-2/src/Hearder.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className='header'>
+        <h1>Conteúdos de Front-End</h1>
+      </header>
+    )
+  }
+}
+
+export default Header
